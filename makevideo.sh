a6c49ae030d4a0ae04cd364fcd10cdd5f8682a02
Kleo <faceplatemusic@gmail.com>
2023-07-10 22:32:05 -0600

    removed script you're not supposed to have

--- makevideo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-ffmpeg -r 50 -i ./frames/FRAME_%d.png -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -vcodec libx264 -crf 25 -pix_fmt yuv420p out.mp4
-
-ffmpeg  -i out.mp4 -i ./resources/test5.wav -c:v copy -map 0:v -map 1:a -y out-audio.mp4
